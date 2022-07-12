1a1a30bc776a6b04dd4f3ad97dc0353ef27f801f
KhunBall <indeeprw61@gmail.com>
2022-07-12 14:07:20 +0700

    update server.go

--- goapi/server.go
+++ goapi/server.go
@@ -4,6 +4,7 @@ import (
 	db "goapi/db"
 	u "goapi/users"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,17 +15,25 @@ func main() {
 	db.Migrate()
 
 	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {}))
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		// AllowOrigins: []string{"http://localhost"},
-		AllowOrigins: []string{"*"},
-		// AllowOrigins: []string{"*", "http://192.168.174.172:4200"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-		// AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization },
-		// AllowMethods: []string{echo.OPTIONS, echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			if strings.HasPrefix(c.Request().Host, "192.168.174.172") {
+				return true
+			}
+			return false
+		},
 	}))
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+	// e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {}))
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// AllowOrigins: []string{"http://localhost"},
+	// AllowOrigins: []string{"*"},
+	// AllowOrigins: []string{"*", "http://192.168.174.172:4200"},
+	// AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	// AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization },
+	// AllowMethods: []string{echo.OPTIONS, echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	// }))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, `{"response": "success"}`)
